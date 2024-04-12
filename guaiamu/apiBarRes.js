c592fb3789fb58010629f9cc9ca96d2383a8e007
Clarice Rodrigues <144385252+clarirodrigues@users.noreply.github.com>
2024-04-11 21:09:29 -0300

    Delete guaiamu directory

--- guaiamu/apiBarRes.js
+++
@@ -1,9 +0,0 @@
-const teste = document.querySelector('#teste')
-
-fetch('./bareserestaurantes.json')
-.then((res) => res.json())
-.then((data) => {
-    data.records.forEach(element => {
-        teste.src += `<a href='https://www.google.com'>${element[2]}</a>`
-    });
-})
